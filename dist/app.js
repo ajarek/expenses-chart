23e566656c51255de398404ea52617248c8db83f
ajarek <ajarek@poczta.onet.pl>
2022-05-22 17:13:05 +0200

    add fetch and dynamic %

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log('Hello World');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let progress = document.querySelectorAll(".progress");
+function getData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("./data.json");
+        const data = yield response.json();
+        data.forEach((element) => {
+            let procent = element.amount;
+            let dayWeek = element.day;
+            progress.forEach((el) => {
+                if (el.id === dayWeek) {
+                    el.style.height = `${procent}%`;
+                }
+            });
+        });
+    });
+}
+getData();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello World');
+let progress = document.querySelectorAll(
+  ".progress"
+) as NodeListOf<HTMLElement>;
+
+async function getData(): Promise<void> {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  data.forEach((element: any) => {
+    let procent = element.amount;
+    let dayWeek = element.day;
+    progress.forEach((el) => {
+      if (el.id === dayWeek) {
+        el.style.height = `${procent}%`;
+      }
+    });
+  });
+}
+getData();
