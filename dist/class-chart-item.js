41d54a19d477b3a19d339a51aac60a5ec6d747e9
ajarek <ajarek@poczta.onet.pl>
2022-05-22 22:41:15 +0200

    refactoring and code order

---
+++ dist/class-chart-item.js
@@ -0,0 +1,16 @@
+export class ChartItem {
+    constructor(container, classContainer) {
+        this.container = container;
+        this.classContainer = classContainer;
+    }
+    render() {
+        const div = document.createElement('div');
+    }
+}
+/*<div class="chart-item">
+<div class="progress-container">
+<div id="sun" class="progress"></div>
+</div>
+<p class="day">sun</p>
+</div>
+*/
