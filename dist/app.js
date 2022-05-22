22e71730458fde66c858ab78c4053aa341ca7bc4
ajarek <ajarek@poczta.onet.pl>
2022-05-22 21:56:16 +0200

    add function renderModal, createModal, clearModal

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,39 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-let progress = document.querySelectorAll(".progress");
-function getData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch("./data.json");
-        const data = yield response.json();
-        data.forEach((element) => {
-            let procent = element.amount;
-            let dayWeek = element.day;
-            progress.forEach((el) => {
-                if (el.id === dayWeek) {
-                    el.style.height = `${procent}%`;
-                }
-            });
+const progress = document.querySelectorAll(".progress");
+async function getData() {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    data.forEach((element) => {
+        let procent = element.amount;
+        let dayWeek = element.day;
+        progress.forEach((el) => {
+            if (el.id === dayWeek) {
+                el.style.height = `${procent}%`;
+                el.dataset.amount = `${procent}%`;
+            }
         });
     });
 }
+function renderModal() {
+    progress.forEach((el) => {
+        el.addEventListener("click", createModal);
+    });
+}
+function clearModal() {
+    const modal = document.querySelector('.modal');
+    if (modal) {
+        modal.remove();
+    }
+}
+function createModal(e) {
+    clearModal();
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.style.bottom = `${e.target.dataset.amount}`;
+    modal.style.transform = `translateY(-10px)`;
+    modal.innerHTML = `$${(e.target.dataset.amount).slice(0, -1)}`;
+    let element = e.target.parentElement;
+    element.appendChild(modal);
+}
 getData();
+renderModal();
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-let progress = document.querySelectorAll(
-  ".progress"
-) as NodeListOf<HTMLElement>;
+const progress = document.querySelectorAll(".progress") as NodeListOf<HTMLElement>;
 
 async function getData(): Promise<void> {
   const response = await fetch("./data.json");
@@ -11,8 +9,32 @@ async function getData(): Promise<void> {
     progress.forEach((el) => {
       if (el.id === dayWeek) {
         el.style.height = `${procent}%`;
+        el.dataset.amount = `${procent}%`;
       }
     });
   });
 }
+function renderModal(){
+ 
+  progress.forEach((el) => {
+    el.addEventListener("click",createModal)
+  })
+}
+function clearModal(){
+  const modal = document.querySelector('.modal') as HTMLElement;
+  if(modal){
+  modal.remove();
+  }
+}
+function createModal(e:any){
+  clearModal();
+      const modal = document.createElement('div') as HTMLElement;
+      modal.classList.add('modal');
+      modal.style.bottom=`${e.target.dataset.amount}`;
+      modal.style.transform=`translateY(-10px)`;
+      modal.innerHTML = `$${(e.target.dataset.amount).slice(0, -1)}`;
+      let element =e.target.parentElement as HTMLElement;
+      element.appendChild(modal);
+}
 getData();
+renderModal();
